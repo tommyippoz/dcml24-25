58d71b990d5f1b00d7f11a4da257eedc0df35836
tommyippoz <tommaso.zoppi@unifi.it>
2024-10-18 16:12:46 +0200

    final version lab 18/10

--- my_first_monitor.py
+++ my_first_monitor.py
@@ -1,18 +1,44 @@
 import csv
 from time import sleep
 import psutil
+import time
 
 # Library for getting indicators -> https://psutil.readthedocs.io/en/latest/
+
+def current_milli_time():
+    """
+    This function returns the current time in milliseconds
+    :return:
+    """
+    return round(time.time() * 1000)
 
 def read_data():
     """
     This function reads data from the system
     :return: a dictionary
     """
+    data_dict = {"time": current_milli_time()}
+    cpu_probe(data_dict)
+    vm_probe(data_dict)
+    return data_dict
+
+def cpu_probe(data_dict):
+    """
+    This function reads CPU data from the system and uses it to update a dict
+    """
     cpu_t = psutil.cpu_times()
-    cpu_dict = {"idle_time": cpu_t.idle, "usr_time": cpu_t.user}
-    cpu_dict["interrupt_time"] = cpu_t.interrupt
-    return cpu_dict
+    data_dict["idle_time"] = cpu_t.idle
+    data_dict["usr_time"] = cpu_t.user
+    data_dict["interrupt_time"] = cpu_t.interrupt
+
+def vm_probe(data_dict):
+    """
+    This function reads VM data from the system and uses it to update a dict
+    """
+    vm_data = psutil.virtual_memory()
+    data_dict["mem_total"] = vm_data.total
+    data_dict["mem_available"] = vm_data.available
+    data_dict["mem_percent"] = vm_data.percent
 
 def write_dict_to_csv(filename, dict_item, is_first_time):
     """
@@ -39,8 +65,8 @@ if __name__ == "__main__":
 
     is_first_time = True
     while True:
-        cpu_dict = read_cpu_usage()
-        write_dict_to_csv("my_first_dataset.csv", cpu_dict, is_first_time)
+        monit_data = read_data()
+        write_dict_to_csv("my_first_dataset.csv", monit_data, is_first_time)
         is_first_time = False
-        print(cpu_dict)
+        print(monit_data)
         sleep(1)
