41780482a4f91b05b69918d9e153526159b8e1d3
tommyippoz <tommaso.zoppi@unifi.it>
2024-10-22 13:49:52 +0200

    added wireshark (pyshark-tshark) probe

--- my_first_monitor.py
+++ my_first_monitor.py
@@ -4,6 +4,8 @@ import psutil
 import time
 
 # Library for getting indicators -> https://psutil.readthedocs.io/en/latest/
+import pyshark as pyshark
+
 
 def current_milli_time():
     """
@@ -11,6 +13,7 @@ def current_milli_time():
     :return:
     """
     return round(time.time() * 1000)
+
 
 def read_data():
     """
@@ -22,6 +25,7 @@ def read_data():
     vm_probe(data_dict)
     return data_dict
 
+
 def cpu_probe(data_dict):
     """
     This function reads CPU data from the system and uses it to update a dict
@@ -31,6 +35,7 @@ def cpu_probe(data_dict):
     data_dict["usr_time"] = cpu_t.user
     data_dict["interrupt_time"] = cpu_t.interrupt
 
+
 def vm_probe(data_dict):
     """
     This function reads VM data from the system and uses it to update a dict
@@ -39,6 +44,24 @@ def vm_probe(data_dict):
     data_dict["mem_total"] = vm_data.total
     data_dict["mem_available"] = vm_data.available
     data_dict["mem_percent"] = vm_data.percent
+
+
+def pyshark_parse(data_dict, pk_capture):
+    """
+    This function parses PyShark data into the existing dict. For the moment it gets only 3 indicators
+    Note: for PyShark to work you have to install wireshark+tshark in your system.
+    """
+    n_packets = 0
+    is_tcp = 0
+    byte_size = 0
+    for packet in pk_capture._packets:
+        n_packets += 1
+        byte_size += int(packet.captured_length)
+        is_tcp += 1 if packet.transport_layer == 'TCP' else 0
+    data_dict["n_packets"] = n_packets
+    data_dict["tcp_packets"] = is_tcp
+    data_dict["packet_size"] = byte_size
+
 
 def write_dict_to_csv(filename, dict_item, is_first_time):
     """
@@ -58,15 +81,21 @@ def write_dict_to_csv(filename, dict_item, is_first_time):
     w.writerow(dict_item)
     f.close()
 
+
 if __name__ == "__main__":
     """
     Main of the monitor
     """
-
     is_first_time = True
     while True:
+        # Monitors PSUtil data
         monit_data = read_data()
+        # Monitor with PyShark for the remaining loop time (replaces sleep)
+        capture = pyshark.LiveCapture(interface='Ethernet 3')
+        capture.sniff(timeout=1)
+        capture.close()
+        # Add PyShark data to dict
+        pyshark_parse(monit_data, capture)
+        print(monit_data)
         write_dict_to_csv("my_first_dataset.csv", monit_data, is_first_time)
         is_first_time = False
-        print(monit_data)
-        sleep(1)
